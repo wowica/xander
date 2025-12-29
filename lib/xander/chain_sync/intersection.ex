ec0f78fbf3e1f4245e01fd0dbecace7437d6e72d
Carlos Souza <carloshrsouza@gmail.com>
2025-12-29 10:57:32 -0500

    Refactor ChainSync to use Transport

--- lib/xander/chain_sync/intersection.ex
+++ lib/xander/chain_sync/intersection.ex
@@ -1,6 +1,7 @@
 defmodule Xander.ChainSync.Intersection do
   alias Xander.Messages
   alias Xander.ChainSync.Response
+  alias Xander.Transport
   alias Xander.ChainSync.Response.RollBackward
   alias Xander.ChainSync.Response.IntersectFound
   alias Xander.ChainSync.Ledger
@@ -9,9 +10,9 @@ defmodule Xander.ChainSync.Intersection do
 
   @recv_timeout 10_000
 
-  def find_target(client, socket, sync_from) do
-    with :ok <- client.send(socket, Messages.next_request()),
-         {:ok, intersection_response} <- client.recv(socket, 0, @recv_timeout),
+  def find_target(transport, socket, sync_from) do
+    with :ok <- Transport.send(transport, socket, Messages.next_request()),
+         {:ok, intersection_response} <- Transport.recv(transport, socket, 0, @recv_timeout),
          {:ok, %RollBackward{tip: tip}} <- Response.parse_response(intersection_response) do
       case sync_from do
         :conway ->
@@ -33,11 +34,11 @@ defmodule Xander.ChainSync.Intersection do
     end
   end
 
-  def find_intersection(client, socket, tslot, tbytes) do
+  def find_intersection(transport, socket, tslot, tbytes) do
     message = Messages.find_intersection(tslot, tbytes)
-    :ok = client.send(socket, message)
+    :ok = Transport.send(transport, socket, message)
 
-    case client.recv(socket, 0, @recv_timeout) do
+    case Transport.recv(transport, socket, 0, @recv_timeout) do
       {:ok, intersection_response} ->
         Logger.debug("Find intersection response successful")
 
